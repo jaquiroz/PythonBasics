325148455f7977127221c255c35a500f683e4128
jaquiroz <jorge.armando.quiroz.perez@gmail.com>
2020-07-04 18:30:38 -0400

    Primer Examen Completo

---
+++ Examen/Examen 1/Examen1.py
@@ -0,0 +1,7 @@
+#There is a function we are providing in for you in this problem called square. 
+#It takes one integer and returns the square of that integer value. Write code to assign 
+#a variable called xyz the value 5*5 (five squared). Use the square function, rather than just multiplying with *.
+num=5
+#xyz=square(num) #square es una funcion propia del curso
+xyz=num*num
+print(xyz)
