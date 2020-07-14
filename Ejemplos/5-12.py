2de3c4f9e77b86c31ca660c240d04c490d21ec3f
jaquiroz <jorge.armando.quiroz.perez@gmail.com>
2020-07-13 20:39:27 -0400

    Primera Semana Completa

---
+++ Ejemplos/5-12.py
@@ -0,0 +1,23 @@
+import turtle
+wn = turtle.Screen()
+bob = turtle.Turtle()
+
+bob.right(60)
+bob.forward(50)
+
+bob.left(60)
+bob.forward(50)
+
+bob.left(60)
+bob.forward(50)
+
+bob.left(60)
+bob.forward(50)
+
+bob.left(60)
+bob.forward(50)
+
+bob.left(60)
+bob.forward(50)
+
+wn.exitonclick()
---
+++ Ejemplos/5-8.py
@@ -0,0 +1,24 @@
+import turtle
+import math
+wn = turtle.Screen()
+bob = turtle.Turtle()
+bob.right(90)
+bob.forward(50)
+bob.left(90)
+bob.forward(50)
+
+# Add your code below!
+bob.left(90)
+bob.forward(50)
+
+bob.left(90)
+bob.forward(50)
+
+dist = math.sqrt(50*50/2)
+bob.right(135)
+bob.forward(dist)
+
+bob.right(90)
+bob.forward(dist)
+
+wn.exitonclick()
