2e6c20b87ceb3c59c97eca1a37422ab47cb9d4d5
jaquiroz <jorge.armando.quiroz.perez@gmail.com>
2020-07-18 21:04:57 -0400

    Tercera Semana Primera Parte

---
+++ Ejemplos/Semana 3/Bool1.py
@@ -0,0 +1,8 @@
+#1
+print(True)
+print(type(True))
+print(type(False))
+#2
+print(5 == 5)
+print(5 == 6)
+
---
+++ Ejemplos/Semana 3/Operadores_in_on.py
@@ -0,0 +1,23 @@
+#in permite buscar un sub string dentro del string total
+print("Ejercicios Operador in")
+print('p' in 'apple')
+print('i' in 'apple')
+print('ap' in 'apple')
+print('pa' in 'apple')
+
+print('a' in 'a')
+print('apple' in 'apple')
+print('' in 'a')
+print('' in 'apple')
+
+#not in busca un sub string que no esta dentro del string total
+print("Ejercicios Operador in")
+print('x' not in 'apple')
+
+#Aplicacion en Listas
+print("Aplicacion en listas")
+print("a" in ["a", "b", "c", "d"])
+print(9 in [3, 2, 9, 10, 9.0])
+print('wow' not in ['gee wiz', 'gosh golly', 'wow', 'amazing'])
+
+
